ac0d80a3797d92683cf816a07e15239d8ae0a3ee
Nicolas Renvoisé <n.renvoise@ubitransport.com>
2020-03-27 11:03:25 +0100

    feat: add obvious php array

---
+++ array/associative_array.php
@@ -0,0 +1,14 @@
+<?php
+
+$fruitOwners = [
+    'bob' => 'apple', 
+    'sarah' => 'banana',
+    'jim' => 'peach'
+];
+
+print_r($fruitOwners);
+
+
+$fruitOwners['jean'] = 'raspberry';
+
+print_r($fruitOwners);
