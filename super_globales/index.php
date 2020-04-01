33564c5c837c8cd3d56c0ce45fbf0d36b8abc397
Nicolas Renvoisé <n.renvoise@ubitransport.com>
2020-04-01 22:15:44 +0200

    feat: add simple uri function

--- super_globales/index.php
+++ super_globales/index.php
@@ -1,4 +1,15 @@
 <?php
+
+/**
+ * Check an URI e.g. /home/zog
+ */
+function isWantedUri(string $regex = '#^/home/zog$#', array &$matches = [])
+{
+    return preg_match($regex, $_SERVER['REQUEST_URI'], $matches);
+}
+
+$uri = $_SERVER['REQUEST_URI'];
+
 $_ENV["hello"] = "world!";
 
 require __DIR__ . "/view.php";
